ad47376ba075ef876dce8462b463f6005f1ec23f
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2021-02-02 19:27:47 -0300

    Fixed a lot of stuff

--- togarashi-card/main.js
+++ togarashi-card/main.js
@@ -1,135 +1,78 @@
-// $("canvas").drawPolygon({
-//   draggable: true,
-//   fillStyle: "#6c3",
-//   x: 100, y: 100,
-//   radius: 50, sides: 5
-// });
-
-let type_colors = {
-  "vazio": "#95a5a6",
-  "vento": "#2ecc71",
-  "terra": "#e67e22",
-  "agua": "#2980b9",
-  "fogo": "#e74c3c"
+let stats_positions = {
+  "cost": [40, 401],
+  "attack": [118, 401],
+  "defense": [196, 401],
+  "speed": [275, 401]
 }
+
+let effect_position = [157, 345];
+let name_position = [127, 35];
+let type_position = [275, 39];
 
 let last_url = "";
 let last_tam = 100;
 let last_rot = 0;
 
-$(function() {
+let curr_layer_name = 0;
+
+function create_circle(pos, fill, stroke){
+  return {
+    type: 'arc',
+    name: `layer_${curr_layer_name++}`,
+    fillStyle: fill,
+    strokeStyle: stroke,
+    strokeWidth: 5,
+    x: pos[0], y: pos[1],
+    radius: 30
+  }
+}
+
+function create_img(src, scale, pos){
+  return {
+    type: 'image',
+    name: `layer_${curr_layer_name++}`,
+    source: src,
+    scale: scale,
+    x: pos[0], y: pos[1],
+  }
+}
+
+function create_text(text, fill, pos){
+  return {
+    type: 'text',
+    name: `layer_${curr_layer_name++}`,
+    fillStyle: fill,
+    fontStyle: 'bold',
+    fontSize: '30pt',
+    fontFamily: 'Trebuchet MS, sans-serif',
+    text: text,
+    x: pos[0], y: pos[1],
+  }
+}
+
+function add_backgrounded_text(text, pos, size, max_w){
   $('canvas').addLayer({
-    type: 'arc',
-    layer: true,
-    groups: ['info-holder'],
-    fillStyle: '#34495e',
-    strokeStyle: '#2c3e50',
-    strokeWidth: 5,
-    x: 43, y: 43,
-    radius: 37
-  }).addLayer({
-    type: 'arc',
-    layer: true,
-    groups: ['info-holder'],
-    fillStyle: '#3498db',
-    strokeStyle: '#2980b9',
-    strokeWidth: 5,
-    x: 35, y: 440 - 35,
-    radius: 30
-  }).addLayer({
-    type: 'arc',
-    layer: true,
-    groups: ['info-holder'],
-    fillStyle: '#e74c3c',
-    strokeStyle: '#c0392b',
-    strokeWidth: 5,
-    x: 315 - 35, y: 440 - 35,
-    radius: 30
-  }).addLayer({
     type: 'text',
-    name: 'card-type-name',
+    name: `layer_${curr_layer_name++}`,
     fillStyle: '#1c1c1c',
-    fontSize: '10pt',
+    fontSize: `${size}pt`,
     fontStyle: 'bold',
     fontFamily: 'Trebuchet MS, sans-serif',
-    text: "agua",
-    x: 315 - 35, y: 35,
-    maxWidth: 170
+    text: text,
+    x: pos[0], y: pos[1],
+    maxWidth: max_w
   }).addLayer({
-    type: 'arc',
-    layer: true,
+    type: 'rectangle',
     index: -1,
-    name: 'card-type',
-    fillStyle: type_colors["agua"],
-    strokeStyle: '#e9e9e9',
-    strokeWidth: 2,
-    x: 315 - 35, y: 35,
-    radius: 30
-  }).addLayer({
-    type: 'image',
-    layer: true,
-    index: -1,
-    source: 'img/atk.png',
-    scale: 0.08,
-    x: 35, y: 440 - 35
-  }).addLayer({
-    type: 'image',
-    source: 'img/def.png',
-    scale: 0.14,
-    x: 315 - 35, y: 440 - 35
-  }).addLayer({
-    type: 'image',
-    layer: true,
-    index: 0,
-    name: 'image-layer',
-    source: "https://i.imgur.com/1az6btP.png",
-    draggable: true,
-    rotate: 15,
-    scale: 1.26,
-    x: 165, y: 220
-  }).addLayer({
-    type: 'text',
-    layer: true,
-    name: 'cost-layer',
-    fillStyle: '#e9e9e9',
-    fontStyle: 'bold',
-    fontSize: '40pt',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: "2",
-    x: 44, y: 47
-  }).addLayer({
-    type: 'text',
-    layer: true,
-    name: 'atk-layer',
-    fillStyle: '#e9e9e9',
-    fontStyle: 'bold',
-    fontSize: '30pt',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: "2",
-    x: 35, y: 440 - 32
-  }).addLayer({
-    type: 'text',
-    layer: true,
-    name: 'health-layer',
-    fillStyle: '#e9e9e9',
-    fontStyle: 'bold',
-    fontSize: '30pt',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: "2",
-    x: 315 - 35, y: 440 - 32
-  }).drawLayers();
-});
+    name: `layer_${curr_layer_name++}`,
+    fillStyle: '#ffffffA8',
+    x: pos[0], y: pos[1],
+    width: $('canvas').measureText(`layer_${curr_layer_name - 2}`).width > 0 ? $('canvas').measureText(`layer_${curr_layer_name - 2}`).width + 10 : 0,
+    height: $('canvas').measureText(`layer_${curr_layer_name - 2}`).height > 0 ? $('canvas').measureText(`layer_${curr_layer_name - 2}`).height + 10 : 0,
+  });
+}
 
-$("#create-card").click(() => {
-  $('canvas')
-    .removeLayer('cost-layer')
-    .removeLayer('atk-layer')
-    .removeLayer('health-layer')
-    .removeLayer('effect-layer')
-    .removeLayer('effect-bg-layer')
-    .removeLayer('card-type')
-    .removeLayer('card-type-name');
-
+function update_background(){
   if($('#card-url').val() != last_url || parseFloat($('#card-rotation').val()) != last_rot || parseFloat($('#card-scale').val()) != last_tam){
     last_url = $('#card-url').val();
     last_rot = parseFloat($('#card-rotation').val());
@@ -145,7 +88,6 @@ $("#create-card").click(() => {
 
     $('canvas').removeLayer('image-layer').addLayer({
       type: 'image',
-      layer: true,
       index: 0,
       name: 'image-layer',
       source: last_url,
@@ -155,74 +97,54 @@ $("#create-card").click(() => {
       x: last_x, y: last_y
     });
   }
+}
 
-  $('canvas').addLayer({
-    type: 'text',
-    layer: true,
-    name: 'cost-layer',
-    fillStyle: '#e9e9e9',
-    fontStyle: 'bold',
-    fontSize: '40pt',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: $('#card-cost').val(),
-    x: 44, y: 47
-  }).addLayer({
-    type: 'text',
-    layer: true,
-    name: 'atk-layer',
-    fillStyle: '#e9e9e9',
-    fontStyle: 'bold',
-    fontSize: '30pt',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: $('#card-atk').val(),
-    x: 35, y: 440 - 32
-  }).addLayer({
-    type: 'text',
-    layer: true,
-    name: 'health-layer',
-    fillStyle: '#e9e9e9',
-    fontStyle: 'bold',
-    fontSize: '30pt',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: $('#card-health').val(),
-    x: 315 - 35, y: 440 - 32
-  }).addLayer({
-    type: 'text',
-    layer: true,
-    name: 'effect-layer',
-    fillStyle: '#1c1c1c',
-    fontSize: '10pt',
-    fontStyle: 'bold',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: $('#card-effect').val(),
-    x: 157, y: 405,
-    maxWidth: 170
-  }).addLayer({
-    type: 'rectangle',
-    index: -1,
-    name: 'effect-bg-layer',
-    fillStyle: '#ffffff80',
-    x: 157, y: 405,
-    width: $('canvas').measureText('effect-layer').width > 0 ? $('canvas').measureText('effect-layer').width + 10 : 0,
-    height: $('canvas').measureText('effect-layer').height > 0 ? $('canvas').measureText('effect-layer').height + 10 : $('canvas').measureText('effect-layer').height,
-  }).addLayer({
-    type: 'text',
-    name: 'card-type-name',
-    fillStyle: '#1c1c1c',
-    fontSize: '10pt',
-    fontStyle: 'bold',
-    fontFamily: 'Trebuchet MS, sans-serif',
-    text: $('#type-select').val(),
-    x: 315 - 35, y: 35,
-    maxWidth: 170
-  }).addLayer({
-    type: 'arc',
-    index: -1,
-    name: 'card-type',
-    fillStyle: type_colors[$('#type-select').val()],
-    strokeStyle: '#e9e9e9',
-    strokeWidth: 2,
-    x: 315 - 35, y: 35,
-    radius: 30
-  }).drawLayers();
+function clear_stuff(){
+  for(let i = 0; i < curr_layer_name; i++){
+    $('canvas').removeLayer(`layer_${i}`).drawLayers();
+  }
+
+  $('canvas').removeLayer(`effect-layer`).drawLayers();
+  $('canvas').clearCanvas();
+}
+
+function fill_stuff(){
+  clear_stuff();
+
+  $('canvas')
+  .addLayer(create_circle(stats_positions["cost"], '#9b59b6', '#8e44ad'))
+  .addLayer(create_circle(stats_positions["attack"], '#e74c3c', '#c0392b'))
+  .addLayer(create_circle(stats_positions["defense"], '#3498db', '#2980b9'))
+  .addLayer(create_circle(stats_positions["speed"], '#2ecc71', '#27ae60'));
+
+  $('canvas')
+  .addLayer(create_img("img/cost.png", 0.22, stats_positions["cost"]))
+  .addLayer(create_img("img/atk.png", 0.2, stats_positions["attack"]))
+  .addLayer(create_img("img/def.png", 0.2, stats_positions["defense"]))
+  .addLayer(create_img("img/atk.png", 0.2, stats_positions["speed"]));
+
+  $('canvas')
+  .addLayer(create_text($('#card-cost').val(), "#e9e9e9", stats_positions["cost"]))
+  .addLayer(create_text($('#card-atk').val(), "#e9e9e9", stats_positions["attack"]))
+  .addLayer(create_text($('#card-health').val(), "#e9e9e9", stats_positions["defense"]))
+  .addLayer(create_text($('#card-speed').val(), "#e9e9e9", stats_positions["speed"]));
+
+  $('canvas')
+  .addLayer(create_img(`img/shadow.png`, 0.1, type_position))
+  .addLayer(create_img(`img/tipos/${$("#type-select").val()}.png`, 0.1, type_position))
+
+  update_background();
+
+  add_backgrounded_text($('#card-effect').val(), effect_position, 10, 280);
+  add_backgrounded_text($('#card-name').val(), name_position, 20, 230);
+
+  $('canvas').drawLayers();
+}
+
+$(function() {
+  fill_stuff();
+});
+
+$("#create-card").click(() => {
+  fill_stuff();
 });
